b4a7c76e264a1f3ca078ab38a2ae5efb46d6517e
gkgkgkgk <gkepets@gmail.com>
2021-03-09 10:57:55 -0500

    color picker

--- src/renderer/Components/MainScreen/ColorPicker/slider.js
+++ src/renderer/Components/MainScreen/ColorPicker/slider.js
@@ -1,21 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import gradient from 'tinygradient';
 
+const g = gradient(['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ff00ff', '#ff0000']).rgb(358);
 
-const Slider = ({ drag = 1 }) => {
+const Slider = ({ startColor = 179 }) => {
     const [mousePos, setMousePos] = useState({ x: null, y: null })
     const [startPos, setStartPos] = useState({ x: null, y: null })
     const [change, setChange] = useState(false);
-    const [pos, setPos] = useState(179);
-    const [lastPos, setLastPos] = useState(179);
+    const [pos, setPos] = useState(startColor);
+    const [lastPos, setLastPos] = useState(startColor);
+    const [color, setColor] = useState(g[startColor]);
 
+    const posRef = React.useRef(pos);
 
     const updateMousePos = ev => {
         setMousePos({ x: ev.clientX, y: ev.clientY });
     };
 
     const updateMouseUp = ev => {
+        console.log(posRef.current);
+        setLastPos(posRef.current);
         setChange(false);
-        setLastPos(pos);
     };
 
     useEffect(() => {
@@ -37,12 +42,14 @@ const Slider = ({ drag = 1 }) => {
             let p = lastPos + Math.round(d / 3);
 
             if (p > 357) {
-                p -= 357;
+                p -= (358);
             } else if (p < 0) {
-                p += 357
+                p += (358);
             }
 
+            setColor(g[p]);
             setPos(p);
+            posRef.current = p;
         }
     }, [mousePos])
 
@@ -52,21 +59,17 @@ const Slider = ({ drag = 1 }) => {
             height: "100px",
             background: change ? `repeating-linear-gradient(
             90deg,
-            rgba(255, 0, 0, 1) 0px,
-            rgba(255, 154, 0, 1) 10%,
-            rgba(208, 222, 33, 1) 20%,
-            rgba(79, 220, 74, 1) 30%,
-            rgba(63, 218, 216, 1) 40%,
-            rgba(47, 201, 226, 1) 50%,
-            rgba(28, 127, 238, 1) 60%,
-            rgba(95, 21, 242, 1) 70%,
-            rgba(186, 12, 248, 1) 80%,
-            rgba(251, 7, 217, 1) 90%,
-            rgba(255, 0, 0, 1) 100%` : 'black', userSelect: 'none',
+            #ff0000 ${pos * 3 - 535}px,
+            #ff00ff ${pos * 3 - 356}px,
+            #0000ff ${pos * 3 - 178}px,
+            #00ffff ${pos * 3}px,
+            #00ff00 ${pos * 3 + 178}px,
+            #ffff00 ${pos * 3 + 356}px,
+            #ff0000 ${pos * 3 + 535}px` : color, userSelect: 'none',
             textAlign: 'center',
             marginLeft: change ? '-535px' : '-50px',
             left: '50%', position: 'fixed',
-            width: change ? "1070px" : "100px",
+            width: change ? "1071px" : "100px",
             transition: 'borderRadius 0.2s, width 0.2s, margin-left 0.2s, height 0.2s, background 0.2s ease-in'
         }}>
             {pos}
