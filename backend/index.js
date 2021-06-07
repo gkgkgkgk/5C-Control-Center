0a1f055ec7e789031adff2f043461109d13c0b9f
Gregory Presser <gpress2222@gmail.com>
2021-06-07 11:59:59 -0400

    middle re-doing backend

--- backend/index.js
+++ backend/index.js
@@ -15,130 +15,57 @@ const { groups } = require("../env/groups.json");
 
 app.use(cors()); // change this to not allow everyone eventually 
 app.use(bodyParser.json());
-let supressData = false;
-
-let count = 0;
-
-const devices = device_keys.map(({ Name, id, key }) => {
-    const device = new TuyAPI({ id, key });
-    console.log("connecting " + Name);
-
-    return ({ Name, device })
-});
-
-(() => {
-    console.log("connecting devices");
-
-    devices.forEach(async d => {
-        await d.device.find().then(() => {
-            console.log("Found " + d.Name);
-            d.device.connect();
-            console.log(d.Name);
-        });
-
-        console.log(count);
-
-        await d.device.on("connected", () => {
-            // console.log("Connected " + d.Name)
-            count++;
-            d.device.on("disconnected", () => { console.log("I ran the disc"); })
-            d.device.on("error", (e) => { console.log("I ran the error"); console.log(e); })
-        });
-
-        console.log(devices.length + " " + count);
-            if (count === devices.length - 1) {
-                http.listen(port, () => console.log(`listening on port ${port}`));
-                console.log(devices);
-                // devices.forEach (({Name, device}) => {
-                //     device.on("data", onData);
-                // });
-            }
-    });
-})();
 
 
-const onData = async data => {
-    if (supressData) return;
-
-    const res = devices.map(async ({ device, Name }) => {
-
-        const { dps } = await device.get({ schema: true });
-        return ({
-            Name,
-            on: dps?.["20"],
-            mode: dps?.["21"],
-            color: dps?.["24"]
-        })
-
+const getDevices = async ()=>{
+    const devices = device_keys.map(({ Name, id, key }) => {
+        const device = new TuyAPI({ id, key });
+        console.log("connecting " + Name);
+        return ({ Name, device })
     })
-    // console.log(await Promise.all(res))
-    io.emit("update", await Promise.all(res));
+    devices.forEach(async ({device,Name})=>{
+        await device.find().catch(e=>{console.error(`Error with ${Name}`)}); 
+        await device.connect(); 
+    });
+    return devices; 
 }
 
-app.get("/turnOffAllLights", (req, res) => {
-    console.log(devices);
+app.get("/turnOffAllLights", async (req, res) => {
+    const devices = await getDevices();
     console.log("turning off all lights");
-    supressData = true;
     devices.forEach(({ device }, i) => {
-        device.set({ dps: 20, set: false }).then(() => { if (i === devices.length - 1) supressData = false; });
+        device.set({ dps: 20, set: false });
     });
-
-
     res.send(true);
 });
-app.get("/turnOnAllLights", (req, res) => {
-    console.log("turning on all lights");
-    supressData = true;
+
+
+app.get("/turnOnAllLights", async (req, res) => {
+    const devices = await getDevices(); 
     devices.forEach(({ device,Name }, i) => {
         console.log(`turning on ${Name}`);
-        device.set({ dps: 20, set: true }).then(() => { if (i === devices.length - 1) supressData = false; });
+        device.set({ dps: 20, set: true });
     })
     res.send(true);
 });
 
 app.get("/isOneLightOn", async (req, res) => {
-    const val = devices.reduce(async (acc, { device }) => {
+    const devices = await getDevices(); 
+    // console.log(devices); 
+    console.log("I have the devices")
+    const val = devices.reduce(async (acc, { device },i) => {
+        console.log(`started with ${i}`);
         const data = await device.get({ dps: 20 });
+        console.log(`finished with ${i}`);
         return ((await acc) || data);
-    }, new Promise((resolve, reject) => resolve(false)))
-    // console.log(`I ran with value ${await val}`); 
+    }, new Promise((resolve, reject) => resolve(false)));
+    console.log(`I ran with value ${await val}`); 
     res.send((await val));
-})
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
-app.post("/changeLights", async (req, res) => {
-    const isGroup = req.body.isGroup;
-    const lights = isGroup ? groups.filter(({ Name }) => req.body.lights.includes(Name)).reduce((acc, { members }) => ([...acc, ...members.filter(name => !acc.includes(name))]), []) : req.body.lights;
-    const newState = req.body.newState;
-    const devicesToUpdate = devices.filter(({ Name }) => lights.includes(Name));
-    supressData = true;
-    devicesToUpdate.forEach({ device }, i => {
-        if (newState === "white") {
-            // need to check for warmth and other parameters in the setting and newState
-            device.set({
-                multiple: true,
-                data: {
-                    "20": true,
-                    "21": "white"
-                }
-            });
-        } else if (newState === "off")
-            device.set({ dps: 20, set: false });
-        else {
-            device.set({
-                multiple: true,
-                data: {
-                    "20": true,
-                    "21": "colour",
-                    "24": newState
-                }
-            });
-        }
-    });
-    res.send(true);
-})
 
+
+
+http.listen(port, () => console.log(`listening on port ${port}`));
 
