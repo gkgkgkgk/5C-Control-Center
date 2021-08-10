5d78818c9998f87bcbe3f588a1655303f742f01a
Gregory Presser <gpress2222@gmail.com>
2021-08-10 14:23:17 -0400

    made it prettier

--- 5c-control-center/src/Components/SleepScreen/clock.js
+++ 5c-control-center/src/Components/SleepScreen/clock.js
@@ -3,28 +3,25 @@ import {getWeather} from '../../HelperFunctions/weather';
 
 
 const Clock = ({ textSize = 7 }) => {
-    const [time, setTime] = useState(new Date().toLocaleTimeString());
-    const [weather,setWeather] = useState(5); 
+    const [time, setTime] = useState(new Date().toLocaleTimeString([],{hour: 'numeric', minute: '2-digit'}));
+    const [weather,setWeather] = useState(0); 
 
-    const updateTime = () => {
-        setTime(new Date().toLocaleTimeString());
-        setTimeout(updateTime, 1000)
-    }
+    const updateTime = () => setTime(new Date().toLocaleTimeString([],{hour: 'numeric', minute: '2-digit'}));
+    
 
     const updateWeather = async ()=>{
         const w = await getWeather(); 
+        console.log(w); 
         setWeather(Math.round(((w.data.main.temp-273.15)*(1.8))+32)); 
-        setTimeout(updateWeather, 10800000)
     }
     useEffect(() => {
-
-        setTimeout(updateTime, 1000);
-        updateWeather(); 
+        updateWeather();
+        let interval1 = setInterval(updateTime, 1000);
+        let interval2 = setInterval(updateWeather, 10800000); 
         
         return function cleanup() {
-            const killId = setTimeout(() => {
-                for (let i = killId; i > 0; i--) clearInterval(i)
-            }, 0)
+            clearInterval(interval1);
+            clearInterval(interval2);
         }
 
     }, [])
@@ -33,7 +30,7 @@ const Clock = ({ textSize = 7 }) => {
         <div style={{ color: 'white', textAlign:'center'}}>
             <div style ={{fontSize: `${textSize}em`}}>{time} </div>
             <br />
-            It is {weather} degrees in New York City
+            It is {weather}°F in New York City
         </div>
     )
 }
