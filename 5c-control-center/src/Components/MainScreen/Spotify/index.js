4c06304db70ff6c206ef51db0abf5cf6c3c299fa
Gregory Presser <gpress2222@gmail.com>
2021-08-15 12:51:21 -0400

    updated scopes

--- 5c-control-center/src/Components/MainScreen/Spotify/index.js
+++ 5c-control-center/src/Components/MainScreen/Spotify/index.js
@@ -3,7 +3,7 @@ import Dashboard from "./Dashboard";
 const {spotify: {clientId}} = require('../../../env/keys.json');
 
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${window.location.href}skipSpotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${window.location.href}skipSpotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20app-remote-control`;
 const Login = ()=>{
     return (
         <div style = {{width: '100%', height: '100%', display: 'flex', justifyContent: 'center'}}>
