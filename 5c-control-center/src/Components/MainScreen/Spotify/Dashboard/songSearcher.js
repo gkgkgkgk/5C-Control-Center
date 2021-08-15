3147fcafbec4cd7a68252773aaa80259f933d526
Gregory Presser <gpress2222@gmail.com>
2021-08-15 19:07:53 -0400

    serach bigger 3

--- 5c-control-center/src/Components/MainScreen/Spotify/Dashboard/songSearcher.js
+++ 5c-control-center/src/Components/MainScreen/Spotify/Dashboard/songSearcher.js
@@ -22,7 +22,7 @@ const SongSearcher = ({spotifyApi,children,correct_device_id: device_id, transfe
 
     return (
         <div>
-            <input type="text" value={text} onChange={onChange} style ={{width: '95%', height:"30px", fontSize: '20px'}} placeholder="search"/>
+            <input type="text" value={text} onChange={onChange} style ={{width: '95%', height:"60px", fontSize: '40px'}} placeholder="search"/>
             <div style={{display: 'flex', justifyContent: text ? 'start': 'center'}}>
                 {text ? results: children}
             </div>
